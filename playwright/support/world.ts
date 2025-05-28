f0db2df38004f1e72e90bcb9274b3bb0958fe5e3
DevAnamTales <Anam-rehman@live.com>
2025-05-28 22:20:20 +0200

    Create world.ts

---
+++ playwright/support/world.ts
@@ -0,0 +1,22 @@
+import { setWorldConstructor } from '@cucumber/cucumber';
+import { Browser, BrowserContext, Page, chromium } from 'playwright';
+
+export class CustomWorld {
+  browser!: Browser;
+  context!: BrowserContext;
+  page!: Page;
+
+  async init() {
+    this.browser = await chromium.launch({ headless: false });
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+  }
+
+  async close() {
+    await this.page.close();
+    await this.context.close();
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
