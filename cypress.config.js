789e926b00f4322885391329d0c95e4e6cf23cbd
DevAnamTales <Anam-rehman@live.com>
2025-05-28 22:15:52 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'cypress';
+import * as dotenv from 'dotenv';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 import preprocessor from '@badeball/cypress-cucumber-preprocessor';
+dotenv.config();
 
 export default defineConfig({
   e2e: {
+    experimentalModifyObstructiveThirdPartyCode: true,
+    chromeWebSecurity: false,
     baseUrl: 'https://www.coop.se',
     specPattern: 'cypress/e2e/**/*.feature',
     supportFile: "cypress/support/e2e.js",
@@ -17,6 +21,10 @@ export default defineConfig({
         })
       );
       return config;
+    },
+  env: {
+      coopEmail: process.env.coopEmail,
+      coopPassword: process.env.coopPassword,
     },
   },
 });
