2c3f6305e2fa83d39be61af1cee34a631695f6ea
DevAnamTales <Anam-rehman@live.com>
2025-05-28 22:19:45 +0200

    Update playwright.config.ts

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    headless: false,
-    baseURL: 'https://www.coop.se',
-  },
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure'
+  }
 });
